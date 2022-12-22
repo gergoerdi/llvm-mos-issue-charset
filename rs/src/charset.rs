f2c323ec4f275116bca29081cfbdfeba1190338e
Gergo ERDI <gergo@erdi.hu>
2022-12-22 10:18:48 +0800

    Use volatile access for charset RAM region https://github.com/llvm-mos/llvm-mos/issues/266#issuecomment-1362015275

--- rs/src/charset.rs
+++ rs/src/charset.rs
@@ -1,17 +1,19 @@
+use core::intrinsics::{volatile_store};
+
+const CHARSET_BASE: *mut u8 = 0xc000 as _;
+
 #[no_mangle]
 pub extern "C" fn make_charset() {
-    let charset = unsafe { core::slice::from_raw_parts_mut(0xc000 as *mut u8, 256 * 8) };
-
     for c in 0..16 {
         for y in 0..4 {
-            charset[c * 8 + y] =
+            unsafe{ volatile_store(CHARSET_BASE.offset(c * 8 + y),
                 (if c & 0x1 != 0 { 0x0f } else { 0x00 }) |
-                (if c & 0x2 != 0 { 0xf0 } else { 0x00 });
+                (if c & 0x2 != 0 { 0xf0 } else { 0x00 })) }
         }
         for y in 4..8 {
-            charset[c * 8 + y] =
+            unsafe{ volatile_store(CHARSET_BASE.offset(c * 8 + y),
                 (if c & 0x4 != 0 { 0x0f } else { 0x00 }) |
-                (if c & 0x8 != 0 { 0xf0 } else { 0x00 });
+                (if c & 0x8 != 0 { 0xf0 } else { 0x00 })) }
         }
     }
 }
